d1477edc6df78f67469dc9cb4d843b87754949ee
Magda Sz <pesahson@gmail.com>
2021-10-22 09:23:46 +0200

    add basic styling to FilteringCountries

--- FilteringCountries/script.js
+++ FilteringCountries/script.js
@@ -1,38 +1,43 @@
-const results = document.querySelector('.results');
-const input = document.querySelector('input');
+const results = document.querySelector(".results");
+const input = document.querySelector("input");
+const spinner = document.querySelector(".spinner-container");
 
 let countries;
-let searchTerm = '';
+let searchTerm = "";
 
-input.addEventListener('input', (e) => {
+const hideSpinner = () => {
+  spinner.style.display = "none";
+};
+
+input.addEventListener("input", (e) => {
   searchTerm = e.target.value;
   showCountries();
- 
-})
+});
 
-
-const fetchCountries = async() => {
-  countries = await fetch("https://restcountries.com/v2/all")
-    .then(res => res.json());
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
 
-const showCountries = async() => {
+const fetchCountries = async () => {
+  countries = await fetch("https://restcountries.com/v2/all").then((res) =>
+    res.json()
+  );
+};
+
+const showCountries = async () => {
   await fetchCountries();
-  // console.log(countries.filter(c => c.name.includes(searchTerm.toLowerCase())))
-
-  results.innerHTML = (
-    countries.filter(c => c.name.toLowerCase().includes(searchTerm.toLowerCase())).map(country => {
+  results.innerHTML = countries
+    .filter((c) => c.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    .map((country) => {
       return `
-      <li>${country.name}</li>
-      `
-    }).join('')
-  )
-  
-}
+      <li class="country-card">
+      <img src="${country.flag}" alt="${country.name}"/>
+      <h3>${country.name}</h3>
+      <p>${numberWithCommas(country.population)}</p></li>
+      `;
+    })
+    .join("");
+  hideSpinner();
+};
 
 showCountries();
-
-
-
-
-
